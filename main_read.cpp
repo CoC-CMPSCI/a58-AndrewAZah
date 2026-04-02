1b1c8d399d506080d74639e135d423537aa5b80f
AndrewAZah <andrew@roguesurge.com>
2026-04-01 19:59:42 -0700

    A58 done

--- main_read.cpp
+++ main_read.cpp
@@ -11,16 +11,27 @@ int main()
     ifstream ifs;
 
     // TODO: open "students.txt" for reading
-
+    ifs.open("students.txt");
     // TODO: check if file opened successfully; print error and exit if not
+    if (!ifs.is_open())
+    {
+        cerr << "Failed to open file." << endl;
+        return 1;
+    }
 
     // TODO: read M from ifs, print "Total M students"
+    ifs >> M;
+    cout << "Total " << M << " students" << endl;
 
     for (int i = 0; i < M; i++)
     {
         // TODO: read stuName, score1, score2 from ifs
+        ifs >> stuName >> score1 >> score2;
         // TODO: compute sum = score1 + score2, avg = sum / 2.0
+        sum = score1 + score2;
+        avg = sum / 2.0;
         // TODO: print Student Name, score1, score2, Sum, Avg
+        cout << "Student Name: " <<stuName << " score1: " << score1 << " score2: " << score2 << " Sum: " << sum << " Avg: " << avg << endl;
     }
     ifs.close();
     return 0;
--- main_write.cpp
+++ main_write.cpp
@@ -9,19 +9,30 @@ int main()
     double score1, score2;
     string stuName;
     ofstream ofs;
+    ofs.open("students.txt");
 
     // TODO: open "students.txt" for writing
 
     // TODO: check if file opened successfully; print error and exit if not
+    if (!ofs.is_open())
+    {
+        cerr << "Failed to open file." << endl;
+        return 1;
+    }
 
     cout << "Enter the total number of students:";
     // TODO: read N from cin, write N to ofs
+    cin >> N;
+    ofs << N << endl;
 
     for (int i = 0; i < N; i++)
     {
         cout << "Enter the student name and two scores: ";
         // TODO: read stuName, score1, score2 from cin; write them to ofs
+        cin >> stuName >> score1 >> score2;
+        ofs << stuName << " " << score1 << " " << score2 << endl;
     }
     // TODO: close ofs
+    ofs.close();
     return 0;
 }
